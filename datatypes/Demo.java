99f00de5d9673edffb106107b4b74e02bf607958
Afsheen-3143 <afsheenaffu123456@gmail.com>
2025-06-21 14:57:55 +0530

    commit all

---
+++ datatypes/Demo.java
@@ -0,0 +1,35 @@
+package Datatype;
+
+public class Demo {
+
+	public static void main(String[] args) {
+	    int q = 0;
+	    byte B = -100;
+	    float F = 0.7673f;
+	    short S = 20356;
+	    long L = 340285450;
+	    double D = 9.9985738949d;
+	    boolean C = true;
+	    char c = 'T';
+	    String renu  = "Teja";
+	    System.out.println(q);
+	    System.out.println(B);
+	    System.out.println(F);
+	    System.out.println(S);
+	    System.out.println(L);
+	    System.out.println(D);
+	    System.out.println(C);
+	    System.out.println(c);
+	    System.out.println(renu);
+        System.out.println(Integer.SIZE);
+        System.out.println(Long.SIZE);
+        System.out.println(Byte.SIZE);
+        
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+	    	
+		// TODO Auto-generated method stub
+
+	}
+
+}
